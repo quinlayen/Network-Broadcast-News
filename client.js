da3f760cbaee58208dd2169d1e1a706536052c63
quinlayen <quinlayen@gmail.com>
2018-02-24 12:48:23 -1000

    broadcast working.  Also blocked message echo to sender

---
+++ client.js
@@ -0,0 +1,13 @@
+const net = require('net');
+const server = net.createConnection(6969, '10.0.1.112', () => {
+  //server.write('client 1 is connected to server');
+  process.stdin.on('data', data => {
+    console.log(data.toString())
+})
+process.stdout.on('data', data =>{
+    server.write(data);
+})
+  server.on('data', data => {
+    console.log(data.toString());
+  });
+});
--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 const net = require('net');
 const PORT = 6969;
+let clientList = [];
+
 const server = net.createServer(client => {
-  console.log('client connected');
+  //const name = client;
+  //console.log(name)
+  clientList.push(client);
+  console.log(`${clientList} is connected`);
+
+  client.on('data', data => {
+    //console.log(data.toString());
+    broadcast(client, data.toString());
+  });
+  //sends a welcome to newly logged in clients
+  //const welcome = `Hello ${name}. Welcome to this amazing Chat Server\n`;
+  //client.write(welcome);
+
+  let broadcast = (sender, message) => {
+    clientList.forEach(clientName => {
+        if(clientName !== sender){
+      clientName.write(message);
+        }
+    });
+  };
+
+
   client.on('end', () => {
     console.log('client disconnected');
   });
 });
+
 server.on('error', err => {
   throw err;
 });
-server.listen(PORT, () => {
-  console.log(`You are connected on port: ${PORT}`);
-});
+server.listen(PORT, () => {});
